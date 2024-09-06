1dc07d4dc1e62e6a8d57fb2ea101d4ea078f0459
Matteo Bucciol <62759388+matteobu@users.noreply.github.com>
2024-09-06 14:30:12 +0200

    fix images preview

--- src/components/template-components/Elements/Formatter/ImageFormatter.tsx
+++ src/components/template-components/Elements/Formatter/ImageFormatter.tsx
@@ -4,13 +4,21 @@ import React from 'react';
 import { getImageThumbnailUrl } from '../../../../utils/template-utils/utils';
 
 const ImageFormatter: React.FC<IImageProps> = ({ value }) => {
-  const image = value[0];
-  const url = getImageThumbnailUrl(image, 256);
-
   return (
-    <div className={styles.imageItem}>
-      <img src={url} alt={value[0]} style={{ width: '50px', marginRight: '10px' }} />{' '}
-    </div>
+    <>
+      {value.map((image, index) => {
+        const url = getImageThumbnailUrl(image, 256);
+        return (
+          <div className={styles.imageItem} key={index}>
+            <img
+              src={url}
+              alt={image} // Use descriptive alt text if you have context
+              style={{ width: '50px', marginRight: '10px' }}
+            />
+          </div>
+        );
+      })}
+    </>
   );
 };
 
